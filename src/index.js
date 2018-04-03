e872705d60dc7b535f004d2419b3c4ef718da3a5
Evan Harrington <ewh010@bucknell.edu>
2018-04-03 03:31:21 -0400

    stuff

--- src/index.js
+++ src/index.js
@@ -4,30 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
